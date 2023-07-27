c85b525513bda8aa743ba1561372ff10fc9342aa
Prince7399 <princetalaviya2000@gmail.com>
2023-07-27 22:00:36 +0530

    Changes in APi respose of user post

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
 require('./db/config');
 const User = require('./db/User');
 const app = express();
 
-// app.use(express.json());
-
+app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 app.post('/signup', async (req, res) => {
-    let user = new User(req.body);
+    // let user = new User(req.body);
+    const { name, email, password } = req.body;
+    let user = new User({ name, email, password });
     console.log('user :>> ', user);
     let result = await user.save();
-    res.send(result);
+    res.send({ _id: result?._id, name: result?.name, email: result?.email });
     // let user = await (new User(req.body)).save();
     // res.send(user);
 });
